ec0d95b60918c69a1ee3bd59db986aeec3d3d202
Paonrat Panjainam <60309550+paonratp@users.noreply.github.com>
2021-10-14 00:17:37 +0700

    Delete Project directory

--- Project/Config.py
+++
@@ -1,3 +0,0 @@
-Channel_secret = "c3fb39e0f3a214bf6f508dfe1390f29a"
-Channel_access_token = "ODBSJAjvFZUNKymu38L3hB05/aBDIsrJwV1565rvqMVIdu7JGBsX50c+feJ3vGRrMr4aBeZFp8J3Qiu0V6gwh9L8vM75RRnpfI+TmGiEEmHD0MMN49gO+NbI0IHELDHuHRkp+wqZs8Jec8HfJkfBiwdB04t89/1O/w1cDnyilFU="
-Basic_ID = "@671xzojs"
